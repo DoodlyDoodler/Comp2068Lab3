b643bdb28c81d476136875f5e82800282c215363
Simarpreet Singh <200413865@student.georgianc.on.ca>
2020-06-12 00:15:56 -0400

    .

--- Lab3/routes/index.js
+++ Lab3/routes/index.js
@@ -4,7 +4,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'My Family' });
+});
+
+router.get('/Simarpreet', function (req, res) {
+    res.render('Simarpreet', { title: 'Simarpreet' });
+});
+
+router.get('/Mom', function (req, res) {
+    res.render('Mom', { title: 'Mom' });
+});
+
+router.get('/Dad', function (req, res) {
+    res.render('Dad', { title: 'Dad' });
+});
+
+router.get('/Brother', function (req, res) {
+    res.render('Brother', { title: 'Brother' });
 });
 
 module.exports = router;
